16702ba47b8f22fb0249ddd56f2af8730555dd35
kuni4210 <kuni4210@naver.com>
2024-11-01 18:38:59 +0900

    feat:update sqlalchemy -> raw query 수정

---
+++ app/sql/__init__.py
@@ -0,0 +1,13 @@
+import pymysql
+
+class DBConnect:
+    @classmethod
+    def get_db(self):
+        return pymysql.connect(
+            user="root",
+            passwd="test1234!",
+            host="127.0.0.1",
+            db="mood_journal_project",
+            charset="utf8",
+            autocommit=True,
+        )
---
+++ app/sql/user_db.py
@@ -0,0 +1,39 @@
+from app.db import DBConnect
+    
+class UserDAO:
+    def insert_user(self, email, password, name):
+        cursor = DBConnect.get_db().cursor()
+        sql_insert = 'insert into users (email, password, name) values (%s, %s, %s)'
+        ret_cnt = cursor.execute(sql_insert, (email, password, name))
+        DBConnect.get_db().close()
+
+        return f'insert OK : {ret_cnt}'
+
+    def get_users(self):
+        ret = []
+        cursor = DBConnect.get_db().cursor()
+        sql_select = 'select * from users'
+
+        try:
+            # 실행
+            cursor.execute(sql_select)
+
+            rows = cursor.fetchall()
+            for row in rows:
+                temp = {
+                    'id': row[0], 
+                    'email': row[1], 
+                    'name': row[3],
+                    }
+                ret.append(temp)
+        except:
+            pass
+
+        finally:
+            DBConnect.get_db().close()
+            
+        return ret
+
+if __name__ == '__main__':
+    user_list = UserDAO().get_users()
+    print(user_list)
