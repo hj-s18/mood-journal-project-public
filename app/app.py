7aac28085461785b16ea023896b528fdcad67d80
YunSeong <gsd1027@naver.com>
2024-11-04 15:04:19 +0900

    auth_route.py 통합 완료

--- app/app.py
+++ app/app.py
@@ -1,12 +1,8 @@
 import logging
 from flask import Flask
 from .route.auth_route import auth_bp
-from .route.sign_route import signup
-from .route.sign_update_route import update_bp
-from .route.login_route import login_bp
 from .route.home_route import home_bp
-from .route.delete_route import delete_bp
-from .route.logout_route import logout_bp
+
 
 def create_app():
     app = Flask(__name__)
@@ -22,16 +18,6 @@ def create_app():
     # 블루프린트 등록
     app.register_blueprint(auth_bp)
     
-    app.register_blueprint(signup)
-    
-    app.register_blueprint(update_bp)
-    
-    app.register_blueprint(login_bp)
-    
     app.register_blueprint(home_bp)
-
-    app.register_blueprint(delete_bp)
-    
-    app.register_blueprint(logout_bp)
     
     return app
